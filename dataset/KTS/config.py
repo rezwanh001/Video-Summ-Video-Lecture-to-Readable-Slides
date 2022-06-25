271764364b3ebc0a8f8fe89ed6d907d3dd42d625
rezwanh001 <rezwanh001@gmail.com>
2022-06-25 06:35:09 +0600

    KTS config update

--- dataset/KTS/config.py
+++ dataset/KTS/config.py
@@ -10,6 +10,7 @@ class CONFIG:
     VIDEO_PATH      =   "../data/part_4_General_embryology_Part_1.mp4" ## OR, "../data/"
     OUTPUT_PATH     =   "../output/part_4_General_embryology_Part_1.h5" ## generated output file (.h5) name
     FRAME_DIR       =   "../output/frames"
-    TRAIN_DATA      =   True
+    TRAIN_DATA      =   False
     PLOT_FIG        =   True
+    IS_SEPARATE     =   False
 
